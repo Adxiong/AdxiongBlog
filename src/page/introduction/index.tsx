d2f9ea29c06ecade4c8add15cf8bc4c7837bf882
adxiong <260245435@qq.com>
2022-10-09 23:37:29 +0800

    feat: introduction page

--- src/page/introduction/index.tsx
+++ src/page/introduction/index.tsx
@@ -4,12 +4,22 @@
  * @Author: Adxiong
  * @Date: 2022-10-07 23:50:12
  * @LastEditors: Adxiong
- * @LastEditTime: 2022-10-07 23:53:33
+ * @LastEditTime: 2022-10-09 23:32:07
  */
 import React from 'react';
+import './index.css';
 
 function Introduction() {
-  return <div>inIntroduction</div>;
+  return (
+    <div className="introduction">
+      <div className="title">Adxiong</div>
+      <div className="content">
+        Hey, I am adxiong, An ordinary programming enthusiast,Work at Baidu.
+        <br />
+        <br />I will publish my daily learn note in here!
+      </div>
+    </div>
+  );
 }
 
 export default Introduction;
